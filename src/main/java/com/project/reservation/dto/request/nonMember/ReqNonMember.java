db0a2b9ccb3506a4540c57b6c2cf69c19bef2821
dewey827 <dldudgus827@gmail.com>
2025-04-14 11:07:23 +0900

    최종 커밋

--- src/main/java/com/project/reservation/dto/request/nonMember/ReqNonMember.java
+++ src/main/java/com/project/reservation/dto/request/nonMember/ReqNonMember.java
@@ -1,25 +1,16 @@
 package com.project.reservation.dto.request.nonMember;
 
 import com.project.reservation.entity.NonMember;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
 
 public class ReqNonMember {
 
     private String name;
     private String phoneNum;
-
-    @Builder
-    public ReqNonMember(String name, String phoneNum){
-        this.name = name;
-        this.phoneNum = phoneNum;
-    }
 
     public static NonMember ofEntity(ReqNonMember reqNonMember){
         return NonMember.builder()
